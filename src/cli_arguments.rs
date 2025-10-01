3aec5b77e6e3bf71e1922c1ef1bb19d666b0867d
Gaspare Armato <gaspare.armato1@gmail.com>
2025-10-01 15:48:27 +0200

    cli arguments parsing tests

--- src/cli_arguments.rs
+++ src/cli_arguments.rs
@@ -9,3 +9,33 @@ pub(crate) struct CliArguments {
     #[arg(short, long)]
     pub target_servers_base_url: String,
 }
+
+#[cfg(test)]
+mod test {
+    use clap::Parser;
+
+    use crate::cli_arguments::CliArguments;
+
+    #[test]
+    fn test_cli_arguments_long_flags() {
+        let args = CliArguments::parse_from([
+            "load-balancer",
+            "--port",
+            "3000",
+            "--target-servers-base-url",
+            "http://localhost:9000",
+        ]);
+
+        assert_eq!(args.port, 3000);
+        assert_eq!(args.target_servers_base_url, "http://localhost:9000");
+    }
+
+    #[test]
+    fn test_cli_arguments_short_flags() {
+        let args =
+            CliArguments::parse_from(["load-balancer", "-p", "3000", "-t", "https://example.com"]);
+
+        assert_eq!(args.port, 3000);
+        assert_eq!(args.target_servers_base_url, "https://example.com");
+    }
+}
